c4b5ad5cd2d4b3018f8736197dfc79defd7170f0
Michael Raimondi <mike@mikeraimondi.com>
2015-08-27 18:50:58 -0400

    Refactor out global vars

--- main.go
+++ main.go
@@ -10,69 +10,95 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var db *sql.DB
+type server struct {
+	DB *sql.DB
+}
 
-func init() {
-	addr := os.Getenv("POSTGRES_PORT_5432_TCP_ADDR")
-	pw := os.Getenv("POSTGRES_PASSWORD")
-	var err error
-	db, err = sql.Open("postgres", "user=postgres password="+pw+" host="+addr+" dbname=postgres sslmode=disable")
+func (s *server) handler() http.Handler {
+	return s.rootHandler()
+}
+
+func (s *server) run(addr string) error {
+	dbAddr := os.Getenv("POSTGRES_PORT_5432_TCP_ADDR")
+	dbPW := os.Getenv("POSTGRES_PASSWORD")
+	db, err := sql.Open("postgres", "user=postgres password="+dbPW+" host="+dbAddr+" dbname=postgres sslmode=disable")
 	if err != nil {
-		log.Fatal("Failure opening database: ", err)
+		return err
+	}
+	s.DB = db
+	if err = s.DB.Ping(); err != nil {
+		return err
+	}
+	httpServer := &http.Server{
+		Addr:    addr,
+		Handler: s.handler(),
 	}
 
-	if err = db.Ping(); err != nil {
-		log.Fatal("Failure connecting to database: ", err)
+	log.Printf("Listening for requests on %s...\n", addr)
+	return httpServer.ListenAndServe()
+}
+
+func (s *server) Close() error {
+	if err := s.DB.Close(); err != nil {
+		log.Println("Failed to close database connection cleanly: ", err)
 	}
+
+	return nil
 }
 
 func main() {
-	http.HandleFunc("/", rootHandler)
-	if err := http.ListenAndServe(":80", nil); err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	server := &server{}
+	defer func() {
+		if err := server.Close(); err != nil {
+			log.Println("Failed to close server: ", err)
+		}
+	}()
+
+	log.Fatal(server.run(":80"))
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		rows, err := db.Query("SELECT name FROM organizations")
-		if err != nil {
-			log.Print(err)
-			http.Error(w, "Internal application error", http.StatusInternalServerError)
-			return
-		}
-		defer rows.Close()
-		var names []string
-		for rows.Next() {
-			var name string
-			if err := rows.Scan(&name); err != nil {
+func (s *server) rootHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			rows, err := s.DB.Query("SELECT name FROM organizations")
+			if err != nil {
 				log.Print(err)
 				http.Error(w, "Internal application error", http.StatusInternalServerError)
 				return
 			}
-			names = append(names, name)
-		}
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		json.NewEncoder(w).Encode(names)
-		return
-	} else if r.Method == "POST" {
-		if err := r.ParseForm(); err != nil {
-			http.Error(w, "Error parsing form", http.StatusBadRequest)
+			defer rows.Close()
+			var names []string
+			for rows.Next() {
+				var name string
+				if err := rows.Scan(&name); err != nil {
+					log.Print(err)
+					http.Error(w, "Internal application error", http.StatusInternalServerError)
+					return
+				}
+				names = append(names, name)
+			}
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			json.NewEncoder(w).Encode(names)
 			return
-		}
-		if len(r.Form["name"]) != 1 {
-			http.Error(w, "Invalid data", http.StatusBadRequest)
+		} else if r.Method == "POST" {
+			if err := r.ParseForm(); err != nil {
+				http.Error(w, "Error parsing form", http.StatusBadRequest)
+				return
+			}
+			if len(r.Form["name"]) != 1 {
+				http.Error(w, "Invalid data", http.StatusBadRequest)
+				return
+			}
+			if _, err := s.DB.Exec("INSERT INTO organizations (name) VALUES ($1)", r.Form["name"][0]); err != nil {
+				log.Print(err)
+				http.Error(w, "Internal application error", http.StatusInternalServerError)
+				return
+			}
+			w.WriteHeader(http.StatusOK)
 			return
+		} else {
+			w.Header().Set("Allow", "GET, POST")
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		}
-		if _, err := db.Exec("INSERT INTO organizations (name) VALUES ($1)", r.Form["name"][0]); err != nil {
-			log.Print(err)
-			http.Error(w, "Internal application error", http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		return
-	} else {
-		w.Header().Set("Allow", "GET, POST")
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
+	})
 }
