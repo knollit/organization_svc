23df50a588c0decd16600310c107adc9bb75065d
Michael Raimondi <mike@mikeraimondi.com>
2015-12-08 20:46:19 -0500

    Add ID to organization

--- db/db.sql
+++ db/db.sql
@@ -1,7 +1,11 @@
 BEGIN;
-CREATE TABLE organizations (
-  name text PRIMARY KEY
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE IF NOT EXISTS organizations (
+  id   BIGSERIAL PRIMARY KEY,
+  name text UNIQUE
 );
+CREATE INDEX ON organizations (name);
 COMMENT ON TABLE organizations IS 'Organizations using the service';
+COMMENT ON COLUMN organizations.id IS 'UUID of organization';
 COMMENT ON COLUMN organizations.name IS 'Unique name of organization';
 COMMIT;
