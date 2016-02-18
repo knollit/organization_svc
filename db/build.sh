ba091e8eebcc83203f4b4dd57187d1f1d48ed0ff
Michael Raimondi <mike@mikeraimondi.com>
2016-02-17 20:34:58 -0500

    Convert DB scripts to Makefile

--- db/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build -t "knollit/"$CIRCLE_PROJECT_REPONAME"_rdbms:latest" .
--- db/push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-repo="knollit/"$CIRCLE_PROJECT_REPONAME"_rdbms:"
-docker tag $repo"latest" $repo$CIRCLE_SHA1
-docker push $repo$CIRCLE_SHA1
-docker push $repo"latest"
